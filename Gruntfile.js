08d0d868829a3586103c28a21cef402af8fbdc4c
Nick Harberg <nick@seamonsterstudios.com>
2014-09-23 14:02:57 -0700

    created Gruntfile.js

---
+++ Gruntfile.js
@@ -0,0 +1,115 @@
+
+
+module.exports = function(grunt) {
+
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+    grunt.initConfig({
+        jshint : {
+            options: {
+                jshintrc : true
+            },
+            all : ['Gruntfile.js', 'server.js', 'app/js/**/*.js']
+        },
+        clean : ['dist'],
+        copy: {
+            all : {
+                expand  : true,
+                cwd     : 'app/',
+                src     : ['*.css', '*.html', '/images/**/*', '!Gruntfile.js'],
+                dest    : 'dist/',
+                flatten : false,
+                filter  : 'isFile'
+            },
+        },
+        browserify : {
+            all : {
+                src  : 'app/js/**/*/js',
+                dest : 'dist/client.js'
+            },
+            options : {
+                transform : ['debowerify'],
+                debug     : true
+            }
+        },
+        sass : {
+            dist : {
+                files : {
+                    'app/dist/style.css' : 'sass/style.scss'
+                },
+                options : {
+                    includePaths : require('node-bourbon').includePaths,
+                    includePaths : require('node-neat').includePaths
+                }
+            }
+        },
+        express : {
+            dev : {
+                options : {
+                    background : true,
+                    script     : 'server.js'
+                }
+            },
+            prod : {
+                options : {
+                    script   : 'server.js',
+                    node_env : 'production'
+                }
+            },
+            test : {
+                options : {
+                    script : 'server.js',
+                }
+            }
+        },
+        watch : {
+            scripts : {
+                files : ['app/js/**/*.js', 'app/bower_components/**/*.js', 'test/**/*.js', 'app/**/*.html'],
+                tasks : ['build']
+            },
+            express : {
+                files   : ['server.js', 'routes/*.js', 'models/*.js'],
+                tasks   : ['server'],
+                options : {
+                    spawn : false
+                }
+            }
+        }
+    });// end grunt.initConfig
+
+    grunt.registerTask('serve', [ 'build', 'express:dev', 'watch' ]);
+    grunt.registerTask('server', 'serve');
+    grunt.registerTask('hint', 'jshint');
+    grunt.registerTask('build', ['clean', 'browserify', 'copy', 'sass']);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}// end module.exports
