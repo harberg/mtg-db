a70228dcf8cd8f76a29968e6b93bda39565b0c29
Nick Harberg <nick@seamonsterstudios.com>
2014-09-30 11:30:32 -0700

    card upload form working.

--- app/js/app/controllers/addCardsController.js
+++ app/js/app/controllers/addCardsController.js
@@ -9,10 +9,19 @@ module.exports = function(app) {
             "cardColor" : ""
         };
 
+        $scope.uploadMessage = false;
+
         $scope.saveCard = function() {
             $http.post("/api/cards", $scope.card)
                 .success(function(data, status, header, config) {
                     console.log("inside the save");
+                    $scope.uploadMessage  = true;
+                    $scope.displayMessage = "You have saved " + $scope.card.cardName;
+                    $scope.card.cardName  = "";
+                    $scope.card.cardPrice = "";
+                    $scope.card.cardSet   = "";
+                    $scope.card.cardColor = "";
+                    $scope.cardSubmitForm.$setPristine();
                 })
                 .error(function(data) {
                     $log.warn(data)
